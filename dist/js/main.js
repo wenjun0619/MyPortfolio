703c07a26e26ca7c9f4ff2fdea98ec2b4ee08445
Wenjun Junlong Jiang Huang <junlong0619@hotmail.com>
2021-06-30 03:20:39 -0400

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,6 +21,7 @@ function toggleMenu(){
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
+        menuBranding.classList.add('show');
         navItems.forEach(function(item) {
             item.classList.add('show');
         });
@@ -31,6 +32,7 @@ function toggleMenu(){
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
+        menuBranding.classList.remove('show')
         navItems.forEach(function(item) {
             item.classList.remove('show');
         });
